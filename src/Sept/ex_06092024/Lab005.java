8669101ab9674afa065b0206da4fdb3071c868bb
Rajashree Malvade <rajashreermalvade@gmail.com>
2024-09-23 18:19:32 +0530

    Add the files

--- src/Sept/ex_06092024/Lab005.java
+++ src/Sept/ex_06092024/Lab005.java
@@ -3,6 +3,10 @@ package Sept.ex_06092024;
 public class Lab005 {
     public static void main(String[] args) {
         System.out.println("This is code");
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
 
 //        This is comment
     }

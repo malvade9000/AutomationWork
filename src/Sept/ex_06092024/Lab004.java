1d444e876918ae662b7b47888f05535096abf058
Rajashree Malvade <rajashreermalvade@gmail.com>
2024-09-23 18:01:31 +0530

    Add the files

---
+++ src/Sept/ex_06092024/Lab004.java
@@ -0,0 +1,19 @@
+package Sept.ex_06092024;
+
+public class Lab004 {
+
+    public static void main(String[] args) {
+        System.out.println("Hi Aniket!");
+    }
+
+}
+
+//This is a comment
+
+/*
+This
+is
+multi
+line
+comment
+ */
